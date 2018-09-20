290b03167a60273c0ef41fac62cdc9775373aa01
Vince Nguyen <vinceng@vinces-macbook-pro.local>
2018-09-20 08:14:12 -0700

    Initial Commit

---
+++ Project Group 10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Group 10
+//
+//  Created by Vince Nguyen on 9/20/18.
+//  Copyright © 2018 Vince Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
