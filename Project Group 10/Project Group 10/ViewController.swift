ec448bb9f7df16342d7d97e346fc3b0bf3d89310
vincephng <31550038+vincephng@users.noreply.github.com>
2018-09-20 17:23:53 -0700

    Add files via upload

---
+++ Project Group 10/Project Group 10/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project Group 10
+//
+//  Created by Vince Nguyen on 9/20/18.
+//  Copyright © 2018 Vince Nguyen. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
